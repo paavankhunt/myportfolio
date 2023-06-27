ce2dfcc97cd9544a3a63d253d750c9cba2e36f43
PaavanKhunt <khuntpaavan2001@gmail.com>
2023-06-27 11:26:41 +0530

    doppler added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,15 @@ import Image from 'next/image';
 import { useCallback, useEffect, useState } from 'react';
 
 export default function Home() {
-  // const fetchData = useCallback(async () => {
-  //   const query = `*[_type == "users"]`;
-  //   const res = await client.fetch(query);
-  //   console.log(res);
-  // }, []);
+  const fetchData = useCallback(async () => {
+    const query = `*[_type == "users"]`;
+    const res = await client.fetch(query);
+    console.log(res);
+  }, []);
 
-  // useEffect(() => {
-  //   fetchData();
-  // }, [fetchData]);
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return (
     <main
